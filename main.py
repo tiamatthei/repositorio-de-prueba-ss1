4ded47eabe1e4f55e8ac1dab559aa1313e50ba6e
Jorge Bejarano <jorge.bejaranovel@gmail.com>
2024-08-19 10:50:02 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# Description: Este es un archivo de prueba para aprender python
+
+from time import sleep, time
+
+
+CONSTANTE = 200
+
+
+
+
+
+print("////////////////////////////////")
+print()
+
+
+
+edad = input("Ingrese su edad: ")
+print("Su edad es: ", edad)
+print("es del tipo", type(edad))
+
+edad = int(float(edad))  # "1.5" -> 1.5 -> 1
+print("Su edad es: ", edad)
+print("es del tipo", type(edad))
+
+
+# edad_es_mayor_de_edad = edad >= 18
+
+
+# respuesta = input("Anda con su carnet? ingrese 'si' o 'no': ")
+# carnet = respuesta == "si"
+
+
+# print("Puede ingresar a la disco:", edad_es_mayor_de_edad and carnet, sep=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print()
+print("////////////////////////////////")
