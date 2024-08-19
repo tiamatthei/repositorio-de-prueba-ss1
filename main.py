8703b234a459df4684f085358bc6c95a73022ab0
Jorge Bejarano <jorge.bejaranovel@gmail.com>
2024-08-19 11:58:12 -0400

    Borré casi todo el main.py estaba malo

--- main.py
+++ main.py
@@ -14,25 +14,6 @@ print()
 
 
 
-# edad = input("Ingrese su edad: ")
-# print("Su edad es: ", edad)
-# print("es del tipo", type(edad))
-
-# edad = int(float(edad))  # "1.5" -> 1.5 -> 1
-# print("Su edad es: ", edad)
-# print("es del tipo", type(edad))
-
-
-# edad_es_mayor_de_edad = edad >= 18
-
-
-# respuesta = input("Anda con su carnet? ingrese 'si' o 'no': ")
-# carnet = respuesta == "si"
-
-
-# print("Puede ingresar a la disco:", edad_es_mayor_de_edad and carnet, sep=" ")
-
-# Esto es un comentario de prueba!
 
 
 
